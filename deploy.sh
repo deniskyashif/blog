3fff180d07eab77d21cb59a5bff80042c256947b
Denis Kyashif <denis.kyashif@gmail.com>
2020-01-22 21:06:05 +0200

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,6 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
 
 git push origin gh-pages
